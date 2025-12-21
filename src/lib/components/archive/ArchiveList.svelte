328a13d8b0d5357b48eb79875f7a873fb6afb9e2
Roti18 <fahrezazamroni@gmail.com>
2025-12-21 20:13:45 +0700

    feat(archive): update archive list component

--- src/lib/components/archive/ArchiveList.svelte
+++ src/lib/components/archive/ArchiveList.svelte
@@ -1,10 +1,5 @@
 <script lang="ts">
-	import {
-		currentItems,
-		selectItem,
-		goBack,
-		navigationPath
-	} from '$lib/stores/archive-navigation';
+	import { currentItems, selectItem, goBack, navigationPath } from '$lib/stores/archive-navigation';
 	import { fade } from 'svelte/transition';
 	import { quintOut } from 'svelte/easing';
 	import type { ArchiveItem } from '$lib/data/archive-data';
@@ -14,7 +9,7 @@
 	}
 </script>
 
-<div in:fade={{ duration: 600, easing: quintOut }}>
+<div in:fade={{ duration: 600, easing: quintOut }} class="max-md:w-full">
 	{#if $navigationPath.length > 0}
 		<div class="mb-4">
 			<button
@@ -32,11 +27,18 @@
 		{#each $currentItems as item}
 			<button
 				on:click={() => handleItemClick(item)}
-				class="group grid grid-cols-[1fr_40px] gap-12 border-b border-white/8 py-8 transition-all duration-450 ease-in-out hover:translate-x-1.5 hover:bg-gray-950 max-md:gap-4"
+				class="group grid grid-cols-[1fr_40px] gap-12 rounded-sm border-b border-white/8 py-4 transition-all duration-450 ease-in-out hover:translate-x-1.5 hover:bg-gray-400/5 max-md:gap-4"
 			>
-				<div class="text-left">
+				<div class="ml-5 text-left">
 					<div class="text-base tracking-[0.08em] text-gray-100">
-						{item.title}
+						{#if item.semesterTitle}
+							{item.semesterTitle}
+							<div class="mt-1 text-xs tracking-widest text-gray-500">
+								{item.semesterDate}
+							</div>
+						{:else}
+							{item.title}
+						{/if}
 					</div>
 				</div>
 				<div
