af276ceb146bb1fae2807beef49cc3e16d5603ca
Roti18 <akuronif@gmail.com>
2025-12-24 01:04:46 +0700

    feat: table ui components

---
+++ src/lib/components/ui/table/TableHead.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	let className: string = '';
+	export { className as class };
+</script>
+
+<th
+	class="px-6 py-3 text-left text-xs font-medium tracking-wider text-zinc-400 uppercase {className}"
+>
+	<slot />
+</th>
